d21ffb0dd816b3a1de98bf5340c1f339852b5006
titanmet <titanmet@mail.ru>
2020-03-21 22:48:11 +0300

    Dependency injection

--- src/main/java/ru/ratnikov/springcourse/MusicPlayer.java
+++ src/main/java/ru/ratnikov/springcourse/MusicPlayer.java
@@ -1,14 +1,51 @@
 package ru.ratnikov.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<Music>();
+//    private Music music;
+    private String name;
+    private int volume;
 
     // Inversion of Control
-    public MusicPlayer(Music music) {
-        this.music = music;
+//    public MusicPlayer(Music music) {
+//        this.music = music;
+//    }
+
+    public MusicPlayer() {
+
     }
 
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+//    public void setMusic(Music music) {
+//        this.music = music;
+//    }
+
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        for (Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
+
     }
 }
--- src/main/java/ru/ratnikov/springcourse/TestSpring.java
+++ src/main/java/ru/ratnikov/springcourse/TestSpring.java
@@ -12,6 +12,10 @@ public class TestSpring {
 
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
+//        System.out.println(musicPlayer.getName());
+//        System.out.println(musicPlayer.getVolume());
+
         musicPlayer.playMusic();
 
         context.close();
